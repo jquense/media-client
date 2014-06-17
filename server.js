4915f8399e15bc973601e05cec338b1365808ea6
theporchrat <theporchrat@gmail.com>
2014-06-17 18:42:58 -0400

    add the rest of the code...

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+  , _ = require('lodash')
+  , app = express();
+
+
+app.use(require('serve-static')(__dirname + '/public'))
+
+app.get('*', function (req, res){
+    res.sendfile(__dirname + '/public/index.html'); 
+})
+
+app.listen(3001)
